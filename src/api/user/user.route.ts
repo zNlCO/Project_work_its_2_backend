0ee01700db9e4cfb6c8bc9625f21009eefe6627e
DevGualti <mirko.gualti@gmail.com>
2025-06-11 11:35:32 +0200

    env  + user route

--- src/api/user/user.route.ts
+++ src/api/user/user.route.ts
@@ -3,5 +3,9 @@ import express from 'express';
 const router = express.Router();    
 
 router.get('/me', auth, fetch())
+router.get('/users', auth, fetch())
+router.post('/register', auth, fetch())
+router.post('/login', auth, fetch())
+router.put('/verify-email', auth, fetch())
 
 export default router;
