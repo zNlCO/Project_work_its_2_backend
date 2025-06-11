762f01a34b66c2820fdd51eb6260b6077c0fbf49
DevGualti <mirko.gualti@gmail.com>
2025-06-11 16:43:00 +0200

    decommentato routes bike-model

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import userRoutes from './user/user.route';
 // import bikeRoutes from './bike/bike.routes';
-// import bikeModelRoutes from './bike-model/bike-model.routes';
+import bikeModelRoutes from './bike-model/bike-model.routes';
 import accessoryRoutes from './accessory/accessory.routes';
 import insuranceRoutes from './insurance/insurance.routes';
 import storeRoutes from './store/store.routes';
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.use('/auth', userRoutes);
 // router.use('/bikes', bikeRoutes);
-// router.use('/bikes-model', bikeModelRoutes);
+router.use('/bikes-model', bikeModelRoutes);
 router.use('/accessories', accessoryRoutes);
 router.use('/insurances', insuranceRoutes);
 router.use('/store', storeRoutes);
