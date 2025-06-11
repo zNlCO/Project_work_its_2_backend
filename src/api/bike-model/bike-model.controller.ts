ae629b02a1343a16e5a1d20f2cfa5cbe58653c81
Nico <nicolo.paolini@itsdigitalacademy.com>
2025-06-11 21:16:44 +0200

    Update bike-model.controller.ts

--- src/api/bike-model/bike-model.controller.ts
+++ src/api/bike-model/bike-model.controller.ts
@@ -4,7 +4,8 @@ import { BikeModelModel } from "./bike-model.model";
 export const fetchAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const bikeModels = await BikeModelModel.find();
-        res.json(bikeModels);
+        res.status(201).json({ 'message': 'Bike model created', 'data': bikeModels });
+
     }
     catch (err) {
         next(err);
@@ -12,20 +13,20 @@ export const fetchAll = async (req: Request, res: Response, next: NextFunction) 
 }
 
 export const insertBikeModel = async (req: Request, res: Response, next: NextFunction) => {
-    
-    if(!req.user?.isOperator)
+
+    if (!req.user?.isOperator)
         return res.status(401);
-        try {
-            const bikeModel = req.body;
-    
-            const existingBigMike = await BikeModelModel.findOne({ descrizione:bikeModel.descrizione,size:bikeModel.size});
-            if (existingBigMike) {
-                return res.status(409).json({ error: 'BikeModel già creato' });
-            }
-    
-            const newBikeModel = await BikeModelModel.create(bikeModel);
-            
-            res.status(201).json({'message':'Bike model created','data':newBikeModel});
+    try {
+        const bikeModel = req.body;
+
+        const existingBigMike = await BikeModelModel.findOne({ descrizione: bikeModel.descrizione, size: bikeModel.size });
+        if (existingBigMike) {
+            return res.status(409).json({ error: 'BikeModel già creato' });
+        }
+
+        const newBikeModel = await BikeModelModel.create(bikeModel);
+
+        res.status(201).json({ 'message': 'Bike model created', 'data': newBikeModel });
     }
 
     catch (err) {
@@ -34,31 +35,31 @@ export const insertBikeModel = async (req: Request, res: Response, next: NextFun
 }
 
 export const modifyBikeModel = async (req: Request, res: Response, next: NextFunction) => {
-    
-    if(!req.user?.isOperator)
+
+    if (!req.user?.isOperator)
         return res.status(401);
-        try {
-            const {prezzo,type,size,elettrica,imgUrl,descrizione} = req.body;
-            const {id}=req.params;
-            
-             const bikemodel = await BikeModelModel.findByIdAndUpdate(
-                    id,
-                    {
-                        prezzo,
-                        type,
-                        size,
-                        elettrica,
-                        imgUrl,
-                        descrizione
-                    },
-                    { new: true, runValidators: true }
-                );
-        
-                if (!bikemodel) return res.status(404).json({ error: 'Accessorio non trovato' });
-            
-                    
-            
-            res.status(201).json({'message':'Bike model modified','data':bikemodel});
+    try {
+        const { prezzo, type, size, elettrica, imgUrl, descrizione } = req.body;
+        const { id } = req.params;
+
+        const bikemodel = await BikeModelModel.findByIdAndUpdate(
+            id,
+            {
+                prezzo,
+                type,
+                size,
+                elettrica,
+                imgUrl,
+                descrizione
+            },
+            { new: true, runValidators: true }
+        );
+
+        if (!bikemodel) return res.status(404).json({ error: 'Accessorio non trovato' });
+
+
+
+        res.status(201).json({ 'message': 'Bike model modified', 'data': bikemodel });
     }
 
     catch (err) {
