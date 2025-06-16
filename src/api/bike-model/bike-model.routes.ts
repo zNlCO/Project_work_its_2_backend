46dc419f9f957790aa456f7612531fadeffa2873
Nico <nicolo.paolini@itsdigitalacademy.com>
2025-06-16 12:19:40 +0200

    delete bike model

--- src/api/bike-model/bike-model.routes.ts
+++ src/api/bike-model/bike-model.routes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { isAuthenticated } from '../utils/auth/authenticated-middleware';
-import { fetchAll, insertBikeModel,modifyBikeModel } from './bike-model.controller';
+import { deleteBikeModel, fetchAll, insertBikeModel, modifyBikeModel } from './bike-model.controller';
 
 const router = express.Router();
 
 // router.get('/:id', auth, fetch())
 router.get('/', fetchAll)
 router.post('/', isAuthenticated, insertBikeModel)
-router.put('/update/:id',isAuthenticated,modifyBikeModel)
-// router.delete('/:id',auth,fetch())
+router.put('/update/:id', isAuthenticated, modifyBikeModel)
+router.delete('/:id', isAuthenticated, deleteBikeModel)
 
 export default router;
